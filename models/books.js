29bcb468ce1bd014cff149b90bdf7074e49290d9
KarimAmador <karimalejandroAC@gmail.com>
2024-07-26 11:40:49 -0600

    feat: add book model

--- models/books.js
+++ models/books.js
@@ -1,1 +1,17 @@
 const { Schema, model } = require('mongoose');
+
+const bookSchema = new Schema({
+  title: {
+    required: true,
+    type: String
+  },
+  comments: [String],
+  commentCount: {
+    type: Number,
+    default: 0
+  }
+});
+
+const Book = model('Book', bookSchema, 'library');
+
+module.exports = Book;
